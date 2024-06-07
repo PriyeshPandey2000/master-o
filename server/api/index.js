1f33105549289fbefe8a2b3f7409572da226e58c
PriyeshPandey2000 <priyeshpandey2000@gmail.com>
2024-06-07 16:20:14 +0530

    .

--- server/api/index.js
+++ server/api/index.js
@@ -6,7 +6,11 @@ const gameRoutes = require('../routes/game');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://master-o-client.vercel.app'  
+}
+
+));
 app.use(bodyParser.json());
 app.use('/api/game', gameRoutes);
 
