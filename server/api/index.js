f9c92a6690642029ada92a2d19c44105d5c1211c
PriyeshPandey2000 <priyeshpandey2000@gmail.com>
2024-06-07 14:56:32 +0530

    .

--- server/api/index.js
+++ server/api/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const gameRoutes = require('./routes/game');
+const gameRoutes = require('../routes/game');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
