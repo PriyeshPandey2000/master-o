d5a4942a45b17a0bc155c40fd53ce4b003765720
PriyeshPandey2000 <priyeshpandey2000@gmail.com>
2024-06-07 16:23:10 +0530

    .

--- server/api/index.js
+++ server/api/index.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: 'https://master-o-client.vercel.app'  
+    origin: 'https://master-o-client.vercel.app/'  
 }
 
 ));
